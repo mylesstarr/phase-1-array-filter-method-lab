8627354fcd9b4eb996f69c9e010e569263338df3
Myles Starr <mylesstarr@gmail.com>
2021-05-04 09:39:44 -0400

    Work in Progres Tuesday May 4 morning

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+const drivers = [ {"name":"driver 1", "hometown": "Chicago" 
+},
+
+{"name":"driver 2", "hometown": "New York" 
+
+}
+]
+
+function findMatching(drivers, name){
+    const filteredDrivers = drivers.filter(name)
+    if (filteredDrivers === "name") 
+    return filteredDrivers
+}
